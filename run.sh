0e1dd24338814f41d60081a3f7b6f9d75704b840
VannTakashi <andreanrevan276@gmail.com>
2025-03-12 00:11:40 +0700

    update script to enable dirty build

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+# Drop Previous Tree
+rm -rf device/xiaomi/gale
+
+# Clone New Tree
+git clone https://github.com/VannTakashi/device_xiaomi_gale -b cartesian device/xiaomi/gale
+
 # Set up build environment
 echo "====== Starting Envsetup ======="
 source build/envsetup.sh || { echo "Envsetup failed"; exit 1; }
