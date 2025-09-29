96481c0bb6f46726a826e405a396439f81ddad01
vannxavero <vannxaveroo@proton.me>
2025-09-29 21:49:36 +0700

    keluar pas udah generate key

--- script.sh
+++ script.sh
@@ -88,5 +88,6 @@ echo "===================================="
 croot && git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
 cd vendor/evolution-priv/keys
 ./keys.sh
+cd ../../..
 lunch lineage_gale-userdebug
 m evolution
