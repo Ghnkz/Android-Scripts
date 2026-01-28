c23c828fb0a358293bf268e07db9c6368e57326c
vannkyle <akunvng58@gmail.com>
2026-01-29 02:15:46 +0700

    run: enable dirty build

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-# Drop Previous Tree
-rm -rf device/xiaomi/gale
+# Drop Pevious Keys
+rm -rf vendor/custom-priv/keys
 
-# Clone New Tree
-git clone https://github.com/VannTakashi/device_xiaomi_gale -b cartesian device/xiaomi/gale
+# Clone New Keys
+git clone https://github.com/VannTakashi/vann_keys -b pos-16 vendor/custom-priv/keys
 
 # Set up build environment
 echo "====== Starting Envsetup ======="
@@ -11,12 +11,13 @@ echo "====== Envsetup Done ======="
 
 # Lunch
 echo "====== Brunch.... ========"
-brunch gale || { echo "Lunch command failed"; exit 1; }
+breakfast gale || { echo "Lunch command failed"; exit 1; }
 echo "===== Lunching done ========"
 
 
 # Build ROM
 echo "===================================="
-echo "        Build Cartesian..."
+echo "        Build PixelOS..."
+m pixelos
 echo "===================================="
  || { echo "Build failed"; }
